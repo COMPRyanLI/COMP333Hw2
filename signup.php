7b89ba3d5785f2b782fbad02fa163873e126d138
lgartrell11 <138685798+lgartrell11@users.noreply.github.com>
2023-10-13 17:00:41 -0400

    Add files via upload
    this file is linked with w_signup.php

---
+++ signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible"
+          content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name ="description" content = "The signup page">
+    <meta name="description" content="">
+    <title>Welcome to the Best Music community</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <?php
+        session_start();
+        if (!isset($_SESSION['username'])) {
+            exit;
+        }
+        $g = $_SESSION['username'];
+        
+    ?>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="index.html">Features</a></li>
+            <li><a href="index.html">Pricing</a></li>
+            <li><a href="index.html">FAQs</a></li>
+        </ul>
+    </nav>
+
+
+    <h1>Success</h1>
+    <hr color="#a01d88"/>
+    <p class = "middle">Your Account has Been Made <?php echo $g ?> !</p>
+    <p class ="middle" ><a href="index.html">Back Home</a> </p>
+      
+    </a>.
+
+</body>
+</body>
+</html>
